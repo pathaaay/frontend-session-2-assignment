cf168acf508f0c4e25767376c8ec64f493461284
pathaaay <aayush.pathak@roimaint.com>
2026-01-21 15:08:56 +0530

    add react router navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import ProductPage from "./pages/product.tsx";
+import HomePage from "./pages/home.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route element={<App />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
 );
